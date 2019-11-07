46418cdf525ee2240b5c806ac3c1da761cffcd3f
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-07 21:36:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,7 +21,6 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -37,6 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
