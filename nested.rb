265d44600953c373030bef17fc5d34eda53a12b4
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-07 21:43:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,8 +82,8 @@ def adding_matz
      }
 
     programmer_hash = :yukihiro_matsumoto => {
-       :known_for => "Ruby",
-        :languages => ["LISP", "C"]
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
      }
 
 end
